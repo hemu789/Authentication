aea2d52fc87cd7086118835700bcbedffdf9745c
hemu789 <87188130+hemu789@users.noreply.github.com>
2024-01-09 01:39:07 +0530

    Connected to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected To MongoDB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 const app = express();
 
-app.listen(3000, () =>{
-    console.log("Server is running on port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
