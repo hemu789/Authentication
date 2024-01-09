5aaaf96a696b732bd2fc26cffc4db0f6c1b5600c
hemu789 <87188130+hemu789@users.noreply.github.com>
2024-01-09 10:21:36 +0530

    Create User Model

---
+++ api/models/User.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    types: String,
+    required: true,
+    unique: true,
+  },
+
+  email: {
+    types: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    types: String,
+    required: true,
+  },
+},{timestamps : true});
+
+const User = mongoose.model ('User', userSchema);
+
+export default User;
